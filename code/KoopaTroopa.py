e987903f0a654fb42a4b90586dd7b099864a9b40
pibe2 <pibe2@illinois.edu>
2016-08-13 19:22:51 -0500

    still expanding

--- code/KoopaTroopa.py
+++
@@ -1,9 +0,0 @@
-from mario1_1 import *
-from GGlobals import *
-from Enemy import Enemy
-
-
-class KoopaTroopa(Enemy):
-    def __init__(self, start_pos, game):
-        super(KoopaTroopa, self).__init__(start_pos, game)
-        self.y_collide_offset = 56
